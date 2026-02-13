e39fe41d0c3572678bb5cefba6fa4f83133b8bef
LeganSar44 <hoangslevan@gmail.com>
2026-02-13 22:37:45 +0700

    Refactor type conversion examples in Go program

--- 03-Programming/Go/Data Types and Variables/Converting_between_types.go
+++ 03-Programming/Go/Data Types and Variables/Converting_between_types.go
@@ -11,8 +11,15 @@ func main() {
 	// var i int = int(f)
 	// fmt.Printf("%v\n", i)
 
-	var i int = 42
-	var s string = strconv.Itoa(i)
-	fmt.Printf("%v", s)
+	//--------CONVERT INT -> STR----------
+	// var i int = 42
+	// var s string = strconv.Itoa(i)
+	// fmt.Printf("%q", s)
+
+	// -----CONVERT STR -> INT ---------
+	var s string = "200"
+	i, err := strconv.Atoi(s)
+	fmt.Printf("%v, %T \n", i, i)
+	fmt.Printf("%v, %T", err, err)
 
 }
