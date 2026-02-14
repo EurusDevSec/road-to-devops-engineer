c7933c3fb77e4a2f3070c0e649eb0af1830b64d3
LeganSar44 <hoangslevan@gmail.com>
2026-02-14 20:55:06 +0700

    chore: add comment for input parameter in user input example

--- 03-Programming/Go/Data Types and Variables/user_input.go
+++ 03-Programming/Go/Data Types and Variables/user_input.go
@@ -8,6 +8,7 @@ func main() {
 	var is_muggle bool
 	var yearOld int
 	fmt.Print("Enter your name & are you a muggle: ")
+	// input parameter
 	fmt.Scanf("%s %t %d", &name, &is_muggle, &yearOld)
 	fmt.Println(name, is_muggle, yearOld)
 
